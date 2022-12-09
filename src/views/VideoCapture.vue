147c12b4e3ff595b746d1b7b1741d5bc800a69dc
黄瑞 <God_sober@163.com>
2022-12-09 15:14:27 +0800

    合并

--- src/views/VideoCapture.vue
+++ src/views/VideoCapture.vue
@@ -1,16 +1,72 @@
 <template>
     <div>
-        视频采集界面
+        <table-custom :mutiSelect="mutiSelect" :loading="loading" :tableData="tableData" :columns="columns">
+        </table-custom>
     </div>
 </template>
 
 <script>
+import tableCustom from '@/components/myComponent/table/tableCustom.vue';
 export default {
     name: 'TtVideoCapture',
+    components: {
+        tableCustom
+    },
 
     data() {
         return {
-            
+            mutiSelect:true,
+            loading: false,
+            tableData: [
+                {ch:"中国",video:""},
+                {nikeName:123,},
+
+            ],
+            columns: [
+                {
+                    prop: 'ch',
+                    label: '国家',
+                    width: 200,
+                    fixed: true,
+                },
+                {
+                    prop: 'video',
+                    label: '视频',
+                },
+                {
+                    prop: 'label',
+                    label: '标签',
+                },
+                {
+                    prop: 'userId',
+                    label: '用户ID',
+                },
+                {
+                    prop: 'videoId',
+                    label: '视频ID',
+                },
+                {
+                    prop: 'play',
+                    label: '播放量',
+                },
+                {
+                    prop: 'comment',
+                    label: '评论量',
+                },
+                {
+                    prop: 'like',
+                    label: '点赞量',
+                },
+                {
+                    prop: 'Time',
+                    label: '抓取时间',
+                },
+                {
+                    prop: 'tate',
+                    label: '状态',
+                },
+               
+            ],
         };
     },
 
