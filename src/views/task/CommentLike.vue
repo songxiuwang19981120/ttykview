c08b9863c4c7c775dbc07a2812b90834f02a808d
zq <3254783858@qq.com>
2022-12-09 14:31:14 +0800

    合并代码

--- src/views/task/CommentLike.vue
+++ src/views/task/CommentLike.vue
@@ -1,29 +1,71 @@
 <template>
-    <div>
-        评论点赞页面
-    </div>
+	<div>
+		<el-card>
+			<!-- 选择 -->
+			<el-select v-model="value" placeholder="请选择状态">
+				<el-option
+					v-for="item in options"
+					:key="item.value"
+					:label="item.label"
+					:value="item.value"
+				>
+				</el-option>
+			</el-select>
+			<!-- 表格 -->
+			<el-table border style="width: 100%; margin-top: 20px">
+				<el-table-column label="创建时间"> </el-table-column>
+				<el-table-column label="任务名称"> </el-table-column>
+				<el-table-column label="任务状态"> </el-table-column>
+				<el-table-column label="任务进度"> </el-table-column>
+				<el-table-column label="操作">
+					<template>
+						<!-- <el-button type="primary" size="small" @click="toVideoReleaseDialog">查看详情</el-button> -->
+					</template>
+				</el-table-column>
+			</el-table>
+			<el-button type="primary" size="small" @click="toCommentLike">查看详情</el-button>
+			<!-- 弹层 -->
+            <CommentLikeDialog :CommentLikeDialog.sync="showDialog"></CommentLikeDialog>
+		</el-card>
+	</div>
 </template>
 
 <script>
-export default {
-    name: 'TtCommentLike',
+	import CommentLikeDialog from './components/CommentLikeDialog.vue';
+	export default {
+		name: 'TtCommentLike',
+		components: {
+			CommentLikeDialog,
+		},
+		data() {
+			return {
+				options: [
+					{
+						value: '已完成',
+						label: '已完成',
+					},
+					{
+						value: '执行中',
+						label: '执行中',
+					},
+					{
+						value: '执行中断',
+						label: '执行中断',
+					},
+				],
+				value: '',
+                showDialog: false
+			};
+		},
 
-    data() {
-        return {
-            
-        };
-    },
+		mounted() {},
 
-    mounted() {
-        
-    },
-
-    methods: {
-        
-    },
-};
+		methods: {
+			toCommentLike() {
+                this.showDialog = true
+            },
+		},
+	};
 </script>
 
-<style lang="stylus" scoped>
-
-</style>
+<style lang="stylus" scoped></style>
--- src/views/task/VideoRelease.vue
+++ src/views/task/VideoRelease.vue
@@ -1,29 +1,110 @@
 <template>
-    <div>
-        视频发布页面
-    </div>
+	<div>
+		<el-card>
+			<!-- 选择 -->
+			<el-select v-model="value" placeholder="请选择状态">
+				<el-option
+					v-for="item in options"
+					:key="item.value"
+					:label="item.label"
+					:value="item.value"
+				>
+				</el-option>
+			</el-select>
+			<!-- 表格 -->
+			<table-custom :loading="loading" :tableData="tableData" :columns="columns"></table-custom>
+			<!-- 弹层 -->
+			<VideoReleaseDialog :VideoDialog.sync="showDialog"></VideoReleaseDialog>
+		</el-card>
+	</div>
 </template>
-
 <script>
-export default {
-    name: 'TtVideoRelease',
+	import VideoReleaseDialog from './components/VideoReleaseDialog.vue';
+	import tableCustom from '@/components/myComponent/table/tableCustom.vue';
+	export default {
+		name: 'TtVideoRelease',
+		components: {
+			VideoReleaseDialog,
+			tableCustom
+		},
+		data() {
+			return {
+				options: [
+					{
+						value: '已完成',
+						label: '已完成',
+					},
+					{
+						value: '执行中',
+						label: '执行中',
+					},
+					{
+						value: '执行中断',
+						label: '执行中断',
+					},
+				],
+				value: '',
+				loading: false,
+				tableData: [],
+				columns: [
+					{
+						prop: 'release_time',
+						label: '创建时间'
+					},
+					{
+						prop: 'task_name',
+						label: '任务名称',
+					},
+					{
+						prop: 'mode',
+						label: '任务状态',
+					},
+					{
+						prop: 'total',
+						label: '任务进度',
+					},
+					{
+						label: '操作',
+					}
+				],
+				showDialog: false
+			};
+		},
 
-    data() {
-        return {
-            
-        };
-    },
+		created() {
+			this.getVideoTasks();
+		},
 
-    mounted() {
-        
-    },
+		mounted() {},
 
-    methods: {
-        
-    },
-};
+		methods: {
+			toVideoReleaseDialog() {
+				this.showDialog = true;
+			},
+			// 获取视频任务列表
+			async getVideoTasks() {
+				try {
+					const res = await this.$api({
+						type: 'getVideotasks',
+						data: {}
+					})
+					console.log(res,'视频列表数据')
+					this.tableData = res.list
+				} catch (error) {
+					console.error(error)
+				}
+			},
+		},
+		watch: {
+			value(newVal) {
+				console.log(newVal);
+			},
+		},
+	};
 </script>
 
 <style lang="stylus" scoped>
-
+.el-select{
+	margin-bottom: 20px
+}
 </style>
